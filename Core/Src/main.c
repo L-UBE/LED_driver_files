b5c2f8a059644a172ba7af2a7fe11bf5c4a0ded7
yi-hsien <56052299+yi-hsien@users.noreply.github.com>
2023-06-05 23:53:48 -0700

    Update main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -13,6 +13,8 @@
   * in the root directory of this software component.
   * If no LICENSE file comes with this software, it is provided AS-IS.
   *
+  * Designed by Eric Hsieh
+  * Last updated on June 5th 2023
   ******************************************************************************
   */
 /* USER CODE END Header */
